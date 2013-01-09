1f34d69b2dbf3a00c38f6d2f0f34c14baff5478a
Eric Clemmons <eric@smarterspam.com>
2013-01-09 14:48:22 -0600

    Formatting on concat

--- tasks/lib/compiler.js
+++ tasks/lib/compiler.js
@@ -15,7 +15,7 @@ module.exports.init = function(grunt) {
   var concat = function(base, files, callback) {
     grunt.utils.async.concatSeries(files, function(file, next) {
       var id        = path.relative(base, file);
-      var template  = '\n  $templateCache.put("<%= id %>", "<%= content %>"\n  );\n';
+      var template  = '\n  $templateCache.put("<%= id %>",\n    "<%= content %>"\n  );\n';
       var cleaned   = grunt.file.read(file).replace(/"/g, '\\"').replace(/\r?\n/g, '" +\n    "');
       var cached    = grunt.template.process(template, {
         id:       id,
