24c97719a3d83dcc3a9ad3a96828d6acbc67065c
Madhusudhan Srinivasa <madhums8@gmail.com>
2013-05-12 03:24:20 +0530

    fix and update tests

--- test/expected/simple_prepend.js
+++ test/expected/simple_prepend.js
@@ -1,4 +1,4 @@
-angular.module("prepend").run(["$templateCache", function($templateCache) {
+angular.module("prepend", []).run(["$templateCache", function($templateCache) {
 
   $templateCache.put("/prepend/simple.html",
     "Howdy there! \\ Your name is \"{{ name }}\".\n"
