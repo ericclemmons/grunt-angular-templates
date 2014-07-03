4805325acbf2a1503d7a4090723e9ce9f80a6a69
Eric Clemmons <eric@smarterspam.com>
2014-07-03 09:35:56 -0500

    Added some whitespace

--- test/expected/html5.js
+++ test/expected/html5.js
@@ -6,7 +6,7 @@ angular.module('html5').run(['$templateCache', function($templateCache) {
     "    <span>\n" +
     "        Self-closing, sucka!\n" +
     "        <br>\n" +
-    "        <img src='path/to/img'>\n" +
+    "        <img src='path/to/img'> Howdy\n" +
     "</div>\n" +
     "\n" +
     "<hr>\n" +
